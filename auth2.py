853164317c04b5e4c9e8ed187acf62ef792be150
Lambol.Michael.Alee <lambol.alee97@gmail.com>
2017-09-08 17:25:03 +0800

    init the repo in order to show them to Kaminec

---
+++ auth2.py
@@ -0,0 +1,92 @@
+import requests
+import json, sys
+from attrdict import AttrDict
+from collections import namedtuple
+
+
+#: The base url for Yggdrassil requests
+AUTH_SERVER = "https://authserver.mojang.com/"
+CONTENT_TYPE = "application/json"
+HEADERS = {"content-type": CONTENT_TYPE}
+Profile = namedtuple("Profile", 'profile, twitch')
+
+
+def make_requests(endpoint, data):
+    req = requests.post(AUTH_SERVER + endpoint, json.dumps(data), 
+        headers=HEADERS, timeout=20)
+    return req
+
+def check_if_failed(req):
+    if res.status_code == requests.codes['ok']:
+        return None
+    anerror = YggdrassilError(status_code=req.status_code)
+    req_json = req.json()
+    if 'error' in req_json and 'errorMessage' in req_json:
+        message = "[Kaniol {status_code}] {error}: '{error_message}'"
+        message = message.format(status_code=str(req.status_code),
+                                 error=req_json["error"],
+                                 error_message=req_json["errorMessage"])
+        anerror.args = (message,)
+        anerror.error = req_json["error"]
+        anerror.errormsg = req_json["errorMessage"]
+        raise anerror
+
+class YggdrassilError(Exception):
+    error = None
+    errormsg = None
+    status_code = None
+    def __init__(self, error=None, errormsg=None, status_code=None):
+        super(YggdrassilError, self).__init__(errormsg)
+        self.error = error
+        self.errormsg = errormsg
+        self.status_code = status_code
+
+class ClientTokenNotMatchedError(Exception):
+    pass
+
+
+class Auth2:
+    def __init__(self, username=None, access_token=None, client_token=None, *, twitch=False):
+        self.username = username
+        self.access_token = access_token
+        self.client_token = client_token
+        self.twitch = twitch
+
+    def login(self, username, password, client_token):
+        payload = {
+                    "agent":{
+                              "name":"Minecraft", 
+                              "version":1
+                            }, 
+                    "username":username, 
+                    "password":password, 
+                    "client_token":client_token,
+                    "requestUser": self.twitch
+                  }
+        req = make_requests('authenticate', payload)
+        check_if_failed(req)
+        resp = AttrDict(req.json())
+        self.username = username
+        self.access_token = resp.accessToken
+        if client_token != resp.clientToken:
+            msg = "The client you given[{client}] != [{get_client}]"
+            msg.format(client=client_token, get_client=resp.clientToken)
+            raise ClientTokenNotMatchedError(msg)
+        else:
+            self.client_token = resp.clientToken
+        try:
+            profile = AttrDict(resp.selectedProfile)
+            twitch = AttrDict(resp.user)
+        except KeyError:
+            self.prof = Profile(profile, None)
+        else:
+            self.prof = Profile(profile, twitch)
+        return True
+
+    def refresh(self):pass
+
+    def validate(self):pass
+
+    def invalidate(self):pass
+
+    def sign_out(self):pass
---
+++ rewritejsontool.py
@@ -0,0 +1,168 @@
+from zipfile import ZipFile as zips
+from collections import namedtuple
+from os.path import join, abspath
+from attrdict import AttrDict
+from itertools import chain
+from shutil import rmtree
+from os import walk, remove
+from string import Template
+from os import name as sysname
+from contextlib import contextmanager
+
+
+platform = {'nt':'windows', 'posix':'linux', 'osx':'osx'}
+nativekey = "natives-{}".format(platform[sysname])
+#namelist = [('libraries', initlib), ('assetIndex', None), ('downloads', None),
+#            ('mainClass', None), ('minecraftArguments', None)]
+
+
+class JsonComp:
+    def __init__(self, comp, init=None):
+        if init is None:
+            init = lambda comp: comp
+        self.comp = init(comp)
+
+    def parse(self, key=None, **args):
+        if key is None:
+            return self.comp
+        else:
+            return key(self.comp, **args)
+
+class JsonManager:
+    def __init__(self, data, namelist=None):
+        self.data = data    #[('libraries','initlib'), ...]
+        if namelist is None:
+            setattr(self,
+                    'rawdata',
+                    JsonComp(self.data, None))
+        else:
+            self.init(namelist)
+
+    def init(self, namelist):
+        for name, func in namelist:
+            setattr(self,
+                    name,
+                    JsonComp(self.data[name], func))
+
+def initlib(libs):
+    natives = []; names = []
+    for i in libs:
+        i = AttrDict(i)
+        if 'natives' in i:
+            name = i.downloads.classifiers[nativekey]["path"]
+            sha1 = i.downloads.classifiers[nativekey]["sha1"]
+            natives.append((name, sha1))
+        else:
+            name = i.downloads.artifact.path
+            sha1 = i.downloads.artifact.sha1
+            names.append((name, sha1))
+    return (natives, names)
+
+def initassets(assets):
+    data = assets['objects']
+    return (objname['hash'] 
+            for objname in data.values())
+
+def parseLibArgs(liblists, **args):
+        alist = []
+        natives, names = liblists
+        libpath = args["libpath"]
+        nativedir = args["nativedir"]
+        gamejar = args["gamejar"]
+
+        addPath = lambda path:join(libpath, path)
+
+        def extractme(native_path, nativedir):
+            """Extracting the native libraries which are in need of starting Minecraft"""
+            zipf = zips(native_path, mode='r')
+            for i in (k for k in zipf.namelist() if "META-INF" not in k):
+                zipf.extract(i, nativedir)
+
+        for name, _ in names:
+            name = addPath(name)
+            alist.append(name)
+        for native_path, _ in natives:
+            native_path = addPath(native_path)
+            extractme(native_path, nativedir)
+
+        alist.append(gamejar)
+        return alist
+
+def parseLibURLs(liblists, **args):
+    tasks = []
+    libpath = args["libpath"]
+    lib_task = namedtuple("lib_task", ['path', 'url', 'sha1'])
+    urlform = "{prefix}{urlpart}"
+    for (path, sha1) in chain(*liblists):
+        url = urlform.format(prefix=args["libprefix"], urlpart=path)
+        path = join(libpath, path)
+        tasks.append(lib_task(path, url, sha1))
+    return tasks
+
+def parseAssetsIndexURLs(assets, **args):
+    index_task = namedtuple('index_task', ['indexes', 'objects', 'id', 'path', 'url', 'sha1'])
+    return index_task(args["asset_index_dir"], 
+                args["asset_obj_dir"],
+                assets.id,
+                join(args["asset_index_dir"], assets.id+'.json'),
+                assets.url,
+                assets.sha1)
+
+def parseGameURLs(maingame, **args):
+    maingame_task = namedtuple("maingame_task", ['path', 'url', 'sha1'])
+    versiondir = args["versiondir"]
+    version = args["version"]
+    clientpath = join(versiondir, version+'.jar')
+    client_task = maingame_task(clientpath, 
+                                maingame.client.url,
+                                maingame.client.sha1)
+    #server = maingame.server.url, maingame.server.sha1
+    return client_task
+
+def parseAssetsURLs(hashes, **args):
+    assetslist = []
+    hash02 = slice(0, 2)
+    obj_task = namedtuple("obj_task", ['path', 'url', 'hash'])
+    for objhash in hashes:
+        urlhash = objhash[hash02]
+        url = "{prefix}{hash02}/{totalhash}".format(
+        prefix=args["assprefix"], hash02=urlhash, totalhash=objhash)
+        path = join(args["asset_obj_dir"], urlhash)
+        assetslist.append(obj_task(path, url, objhash))
+    return assetslist
+
+def parseMcArgs(mcArgs, **args):
+    mcArgs = Template(mcArgs)
+    mcArgs = mcArgs.substitute(
+            auth_player_name=args["user_name"],
+            version_name= args["version"],
+            game_directory="\"{}\"".format(args["gamepath"]),
+            assets_root="\"{}\"".format(args["assets_root"]),
+            assets_index_name=args["assetsID"],
+            auth_uuid=args["uuid"],
+            auth_access_token=args["accessToken"],
+            user_type=args["gamemode"],
+            version_type="\"LAminec AlphaV2.8\"")
+    return mcArgs
+
+@contextmanager
+def CalJson(javapath, mainclass, nativedir, libpath, mcArgs):
+    command = javapath \
+            + "-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump "\
+            + "-XX:+UseG1GC "\
+            + "-XX:-UseAdaptiveSizePolicy "\
+            + "-XX:-OmitStackTraceInFastThrow "\
+            + "-Xmn128m -Xmx1024m "\
+            + "-Djava.library.path={} ".format(nativedir)\
+            + "-Dfml.ignoreInvalidMinecraftCertificates=true "\
+            + "-Dfml.ignorePatchDiscrepancies=true "\
+            + "-cp {} {} {} --height 480 --width 854".format(libpath, mainclass, mcArgs)
+    try:
+        yield command
+    finally:
+        try:
+            for root, dirs, files in walk(nativedir):
+                for f in files:
+                    remove(join(root, f))
+            rmtree(nativedir)
+        except:pass
